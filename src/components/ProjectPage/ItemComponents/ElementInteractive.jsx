20eb37d7f1ea664c94057fa8077aa7e661f43b3d
echo <play3step@naver.com>
2024-01-30 16:39:25 +0900

    feat: 도형 생성 커스텀 훅으로 분리

--- src/components/ProjectPage/ItemComponents/ElementInteractive.jsx
+++ src/components/ProjectPage/ItemComponents/ElementInteractive.jsx
@@ -1,42 +1,15 @@
 import styled from 'styled-components';
-import { useRecoilState } from 'recoil';
+import useShapes from '../../../hooks/useShapes';
+
 import ElementItem from './ItemContainer/ElementItem';
 import CloseBtn from '../../button/CloseBtn';
-import { shapeList } from '../../../store/recoil';
 
 function ElementInteractive({ onClose }) {
-  const [shapeValue, setShapeValue] = useRecoilState(shapeList);
-
-  const handleAddShape = (shapeType) => {
-    let newShape;
-    const randomX = 600 + (Math.random() * 60 - 30);
-    const randomY = 300 + (Math.random() * 60 - 30);
-    if (shapeType === 'Rectangle') {
-      newShape = {
-        type: 'Rectangle',
-        x: randomX,
-        y: randomY,
-        width: 100,
-        height: 100,
-        fill: 'blue',
-        id: `rect${shapeValue.length + 1}`,
-      };
-    } else if (shapeType === 'Circle') {
-      newShape = {
-        type: 'Circle',
-        x: randomX,
-        y: randomY,
-        radius: 50,
-        fill: 'green',
-        id: `circle${shapeValue.length + 1}`,
-      };
-    }
-    setShapeValue([...shapeValue, newShape]);
-  };
+  const { addShape } = useShapes();
   return (
     <ShapeInteractiveContainer>
       <ShapeItemContainer>
-        <ElementItem onAddShape={handleAddShape} />
+        <ElementItem onAddShape={addShape} />
       </ShapeItemContainer>
       <ClosePostion>
         <CloseBtn onClose={onClose} />
