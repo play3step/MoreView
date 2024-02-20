5c4cb97ef8a75a8242e3fb0690836eeeda1563d0
echo <play3step@naver.com>
2024-02-20 11:18:19 +0900

    feat: 기본 인스턴스 사용 코드 작성

--- src/apis/index.js
+++ src/apis/index.js
@@ -2,13 +2,10 @@ import axios from 'axios';
 
 export const BASE_URL = process.env.REACT_APP_SERVER_URL;
 
-const instance = axios.create({
-  baseURL: 'https://api.themoviedb.org/3',
-  params: {
-    api_key: BASE_URL,
-    language: 'ko-KR',
-  },
-  headers: {},
+const basicApi = axios.create({
+  baseURL: BASE_URL,
+  timeout: 5000,
+  headers: { 'Content-Type': 'application/json;charset=utf-8' },
 });
 
-export default instance;
+export default basicApi;
