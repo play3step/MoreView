c869a0ce9d5a55e0208ba460dbea45dea8697f6f
echo <play3step@naver.com>
2024-03-04 13:42:30 +0900

    feat/ 마이페이지 라우트 지정

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 
 import './App.css';
+import MyPage from './pages/MyPage';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="/myPage" element={<MyPage />} />
           <Route path="/project" element={<ProjectPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
---
+++ src/pages/MyPage.jsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+function MyPage() {
+  return <ListContinaer />;
+}
+
+export default MyPage;
+
+const ListContinaer = styled.div`
+  width: 16.5625vw;
+  height: 100vh;
+  background-color: #f8f8f8;
+`;
