fb53d0bba67314a9b295bf2263492dd9b83d4f16
echo <play3step@naver.com>
2024-03-07 15:08:59 +0900

    feat: route 수정정

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
 import ProjectPage from './pages/ProjectPage';
-import MainPage from './pages/MainPage';
+// import MainPage from './pages/MainPage';
 import LoginPage from './pages/LoginPage';
 
 import './App.css';
@@ -12,9 +12,9 @@ function App() {
     <RecoilRoot>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/myPage" element={<MyPage />} />
-          <Route path="/project" element={<ProjectPage />} />
+          {/* <Route path="/" element={<MainPage />} /> */}
+          <Route path="/" element={<MyPage />} />
+          <Route path="/project/:projectId" element={<ProjectPage />} />
           <Route path="/login" element={<LoginPage />} />
         </Routes>
       </BrowserRouter>
