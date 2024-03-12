f77e3e23d5c4fd730c1d58d155eaf27662afa36b
echo <play3step@naver.com>
2024-03-13 00:07:31 +0900

    Feat: w,s,a,d,shift,control을 이용한 카메라 이동 추가

--- src/components/ProjectPage/PageData/Project3d.jsx
+++ src/components/ProjectPage/PageData/Project3d.jsx
@@ -1,14 +1,62 @@
 import React, { useEffect, useRef } from 'react';
-import { useLoader } from '@react-three/fiber';
+import { useLoader, useThree } from '@react-three/fiber';
 import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
 import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader';
+import { Vector3 } from 'three';
 import userObj from '../../../assets/3dObject/home3d.obj';
 
 function Model() {
   const obj = useLoader(OBJLoader, userObj);
+  const modelRef = useRef();
+  const { camera } = useThree();
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (!modelRef.current) return;
+      const speed = 0.5;
+
+      const forward = new Vector3(0, 0, -1).applyQuaternion(camera.quaternion);
+      const right = new Vector3(1, 0, 0).applyQuaternion(camera.quaternion);
+
+      switch (event.key) {
+        case 'w':
+          modelRef.current.position.add(forward.multiplyScalar(-speed));
+          break;
+        case 's':
+          modelRef.current.position.add(forward.multiplyScalar(speed));
+          break;
+        case 'a':
+          modelRef.current.position.add(right.multiplyScalar(speed));
+          break;
+        case 'd':
+          modelRef.current.position.add(right.multiplyScalar(-speed));
+          break;
+        default:
+          break;
+      }
+    };
+
+    const handleKeyUp = (event) => {
+      if (!modelRef.current) return;
+      const speed = 0.5;
+      if (event.key === 'Shift') {
+        modelRef.current.position.add(new Vector3(0, -speed, 0));
+      } else if (event.key === 'Control') {
+        modelRef.current.position.add(new Vector3(0, speed, 0));
+      }
+    };
+
+    window.addEventListener('keydown', handleKeyDown);
+    window.addEventListener('keyup', handleKeyUp);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+      window.removeEventListener('keyup', handleKeyUp);
+    };
+  }, [camera]);
 
   return (
-    <mesh>
+    <mesh ref={modelRef}>
       <primitive object={obj} position={[0, 0, 0]} scale={0.5} />
     </mesh>
   );
