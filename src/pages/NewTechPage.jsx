872e555b8c4b5f862232971e402b18735e9608ff
echo <play3step@naver.com>
2024-07-31 16:37:48 +0900

    stlye: NewTech 페이지 퍼블리싱

--- src/pages/NewTechPage.jsx
+++ src/pages/NewTechPage.jsx
@@ -1,5 +1,34 @@
+import styled from 'styled-components';
+import TestObject from '../components/myPage/TestObject';
+import DescriptionBox from '../components/NewTechPage/DescriptionBox';
+
 function NewTechPage() {
-  return <div>테크 페이지</div>;
+  return (
+    <div
+      style={{
+        width: '100%',
+        height: '100%',
+
+        padding: '18.148vh 11.481vh 25.92vh 4.0625vw',
+      }}
+    >
+      <NewTechContainer>
+        <DescriptionBox />
+        <TestObject />
+      </NewTechContainer>
+    </div>
+  );
 }
 
 export default NewTechPage;
+
+const NewTechContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 5.55vh 8.854vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 3.125vw;
+  position: relative;
+`;
